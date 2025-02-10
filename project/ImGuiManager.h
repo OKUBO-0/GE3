4975f340b7f66014a027361a82576b42a0d5400c
OKUBO-0 <k023g0020@g.neec.ac.jp>
2025-02-10 17:06:13 +0900

    修正

--- project/ImGuiManager.h
+++ project/ImGuiManager.h
@@ -1,15 +1,15 @@
-#pragma once
-
-#include "DirectXCommon.h"
-#include "WinApp.h"
-
-class ImGuiManager
-{
-public:
-	// 初期化
-	void Initialize(DirectXCommon* dxCommon, WinApp* winapp);
-
-private:
-	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>srvHeap_;
-	//DirectXCommon* dxCommon = nullptr;
-};
+//#pragma once
+//
+//#include "DirectXCommon.h"
+//#include "WinApp.h"
+//
+//class ImGuiManager
+//{
+//public:
+//	// 初期化
+//	void Initialize(DirectXCommon* dxCommon, WinApp* winapp);
+//
+//private:
+//	Microsoft::WRL::ComPtr<ID3D12DescriptorHeap>srvHeap_;
+//	//DirectXCommon* dxCommon = nullptr;
+//};
